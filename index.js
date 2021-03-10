9697dc0fc18bea558e3c1e0382b44aed9ce16e9c
Troy Wills <troy_wills@outlook.com>
2021-03-09 19:51:23 -0600

    object/ array created

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const inquirer = require("inquirer");
 // const jest = require("jest");
 const fs = require("fs");
 const path = require("path");
+const Employee = require("./lib/Employee")
+
+var teamMembers = [];
 
 // User input for constructing team 
 const questions = [
@@ -10,11 +13,6 @@ const questions = [
         type: "input",
         name: "Name",
         message: "What is your team members name?",
-    },
-    {
-        type: "input",
-        name: "Title",
-        message: "What is their title?",
     },
     {
         type: "input",
@@ -28,16 +26,44 @@ const questions = [
     },
 ];
 
+
 // Initialize user input prompts in command line 
-function init() {
+function createTeam() {
     inquirer.prompt(questions).then(responses => {
-        writeToFile("index.html", generatedWebPage(responses))
+        var newEmployee = responses;
+        teamMembers.push(newEmployee);
+        console.log(teamMembers);
+        addEmployee();
     });
 };
 
+const newEmployee = [
+        {
+        type: "list",
+        name: "addEmployee",
+        message: "Do you have another team member to add?",
+        choices: ["yes", "no"]
+    }
+]
+
+ function addEmployee() {
+     inquirer.prompt(newEmployee).then(responses => {
+        
+        if (responses.addEmployee === "yes") {
+            createTeam();
+        }
+        else {
+            // return; 
+              // generate html page
+        }
+         
+     });
+ }
+
 // Write data from user input to HTML 
 function writeToFile(fileName, data) {
+console.log(data);
+};
 
-}
+createTeam();
 
-init();
